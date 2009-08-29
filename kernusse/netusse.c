b026f007863a10bdf2dc5d01c829e725de6d387d
Clément LECIGNE <clemun@gmail.com>
2009-08-30 01:13:25 +0200

    huge improvments.

--- kernusse/netusse.c
+++ kernusse/netusse.c
@@ -1,20 +1,25 @@
 /*
  * netusse.c - fucking kernel net stacks fuzzer
  *
- * Copyright (c) Clément Lecigne, 2006
+ * Copyright (c) Clément Lecigne, 2006-2009
  */
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
-#include <netinet/in.h>
+#include <sys/mman.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <getopt.h>
 #include <string.h>
 #include <sys/klog.h>
+#define _GNU_SOURCE
+#include <fcntl.h>
 #include <net/if_arp.h>
 #include <linux/atalk.h>
+#include <linux/in.h>
+
+#define SEED_FILE "/tmp/netusse.seed"
 
 #ifndef LLC_SAP_NULL
 #define LLC_SAP_NULL 0x00
@@ -45,7 +50,15 @@ static int linux_hasoopsed()
     char    buf[2048];
     int     sz;
     if ( (sz = klogctl(0x3, buf, 2048)) > -1 )
-        return strstr(buf, "ecx: ") != NULL;
+    {
+        if (strstr(buf, "ecx: ") != NULL)
+            return 1;
+
+        /* sk_free: optmem leakage (28 bytes) detected. */
+        if (strstr(buf, "leakage") != NULL)
+            return 1;
+    }
+
     return 0;
 }
 
@@ -111,6 +124,10 @@ static int evilint(void)
         case 9: return 4;
         case 10: return 8;
         case 11: return 12;
+        case 12:
+        case 13:
+        case 14:
+                 return rand() & 32;
         default:
                 return rand() & 2048;
 
@@ -127,6 +144,7 @@ static void dump(unsigned char * data, unsigned int len)
         "....................................................."
         "........................................";
 
+    printf("\n");
     for ( dp = 1; dp <= len; dp++ )
     {
         printf("%02x ", data[dp-1]);
@@ -147,7 +165,7 @@ static void dump(unsigned char * data, unsigned int len)
 void soopt(int s)
 {
 	unsigned int optval;
-	int optlen, optname, level, ret, on = rand() % 2;
+	int optlen, optname, level, ret, on = rand() % 2, tout = 50;
 	do
 	{
 		switch (rand() % 15)
@@ -191,7 +209,7 @@ void soopt(int s)
 
 		ret = setsockopt(s, level, optname, (void *)optval, optlen);
 	}
-    while(ret == -1);
+    while(ret == -1 && tout--);
 }
 
 void gsn(int s)
@@ -327,25 +345,284 @@ void goof(int s)
 	return;
 }
 
+static void fuzzer(char *mm, size_t mm_size)
+{
+    size_t i;
+
+    for ( i = 0 ; i < mm_size ; i++ )
+        mm[i] = rand() & 255;
+
+    return;
+}
+
+/**
+ * fucking bindusse #@!
+ */
+void bindusse(int fd)
+{
+    int     ret, tout = 5, len;
+    char    *b;
+
+    do
+    {
+        len = evilint();
+        b = malloc(len);
+        if ( b != NULL && len < 0xFFFFF)
+            fuzzer(b, len);
+        ret = bind(fd, (struct sockaddr *)&b, len);
+        if (ret && (rand() % 2))
+            listen(fd, rand());
+        free(b); b = NULL;
+    }
+    while (ret < 0 && tout--);
+}
+
+void connectusse(int fd)
+{
+    int     ret, tout = 5, len;
+    char    *b;
+
+    do
+    {
+        len = evilint();
+        b = malloc(len);
+        if ( b != NULL && len < 0xFFFFF)
+            fuzzer(b, len);
+        ret = connect(fd, (struct sockaddr *)&b, len);
+        free(b); b = NULL;
+    }
+    while (ret < 0 && tout--);
+}
+
+void sendmsgusse(int fd)
+{
+	struct msghdr   msg;
+	struct cmsghdr  *cmsg = NULL;
+	struct iovec    iov;
+    char            *b;
+
+    msg.msg_controllen = (rand() % 2) ? evilint() : 0;
+    if (msg.msg_controllen)
+    {
+        if (msg.msg_controllen < sizeof (struct cmsghdr))
+            cmsg = (struct cmsghdr *)malloc(sizeof (struct cmsghdr));
+        else
+            cmsg = (struct cmsghdr *)malloc(msg.msg_controllen);
+        if (cmsg == NULL) goto nocmsghdr;
+        msg.msg_control = cmsg;
+        cmsg->cmsg_level = (rand() % 2) ? IPPROTO_IP : evilint();
+        cmsg->cmsg_type = (rand() % 2) ? rand() % 255 : evilint();
+        cmsg->cmsg_len = (rand() % 2) ? msg.msg_controllen : evilint();
+    }
+    else
+    {
+nocmsghdr:
+        msg.msg_control = (rand() % 5) ? NULL : (void*)evilint();
+        msg.msg_controllen = (rand() % 2) ? rand() : 0;
+    }
+    iov.iov_len = (rand() % 2) ? evilint() : 1;
+    iov.iov_base = ((rand() % 5) == 0) ? (void*)evilint() : &msg;
+    msg.msg_iov = ((rand() % 5) == 0) ? (void*)evilint() : &iov;
+    if (rand() % 10)
+    {
+        msg.msg_namelen = evilint();
+        b = malloc(msg.msg_namelen);
+        if ( b != NULL && msg.msg_namelen < 0xFFFFF)
+            fuzzer(b, msg.msg_namelen);
+        msg.msg_name = b;
+    }
+    else
+    {
+        msg.msg_name = (caddr_t)evilint();
+        msg.msg_namelen = evilint();
+    }
+    msg.msg_flags = evilint();
+    sendmsg(fd, &msg, MSG_DONTWAIT);
+}
+
+void splicusse(int fd)
+{
+    unsigned int    flags;
+    off_t           offin, offout;
+    size_t          len;
+    int             pipes[2], fdout, fdin;
+
+    fdin = fdout = fd;
+    if (rand() % 3)
+    {
+        if (pipe(pipes) == 0)
+            fdout = pipes[rand() % 2];
+    }
+    else
+    {
+        if (rand() % 2)
+        {
+            if (pipe(pipes) == 0)
+                fdin = pipes[rand() % 2];
+        }
+    }
+
+    switch (rand() % 5)
+    {
+        case 0:
+            flags = evilint();
+            break;
+        default:
+            flags = evilint() % 8;
+            break;
+    }
+
+    switch (rand() % 5)
+    {
+        case 0:
+            offin = evilint();
+            offout = evilint();
+            break;
+        default:
+            offin = offout = 0;
+            break;
+    }
+
+    switch (rand() % 5)
+    {
+        case 0:
+            len = evilint();
+            break;
+        default:
+            len = evilint() % 128;
+            break;
+    }
+
+    splice(fd, offin, fdout, offout, len, flags);
+}
+
+void mmapusse(int fd)
+{
+    void    *addr, *raddr;
+    int     flags, prot;
+    size_t  len, off;
+
+    switch (rand() % 5)
+    {
+        case 0:
+            flags = evilint();
+            prot  = evilint();
+            break;
+        default:
+            flags = evilint() % 0x20;
+            prot = evilint() % 0x4;
+            break;
+    }
+
+    switch (rand() % 5)
+    {
+        case 0:
+            addr = (void *)evilint();
+            flags |= MAP_FIXED;
+            break;
+        default:
+            addr = NULL;
+            flags &= ~MAP_FIXED;
+            break;
+    }
+
+    raddr = mmap(addr, (len=evilint()), prot, flags, fd, (off=evilint()));
+
+    if ( raddr != MAP_FAILED )
+    {
+        printf("\nmmap(%p, %u, %d, %d, %d (snum = %d), %d); -> %p\n", addr, len, prot, flags, fd, snum, off, raddr);
+    }
+}
+
 void usage(char *prog)
 {
-	printf(" - usage: %s [-r seed][-n occ][-o occ]\n", prog);
+    printf("NETUSSE. KERNEL NET STACK DESTROYER.\n");
+    printf("0xbadc0ded by Clement LECIGNE <root @ clem1.be>.\n\n");
+	printf("\tusage: %s [-r seed][-n occ][-o occ][-f] && rm -fr /\n", prog);
+    printf("RTFS. RTFS. RTFS.\n");
 	exit(EXIT_FAILURE);
 }
 
 int main(int ac, char **av)
 {
 	char c;
-	int s, i, opts;
+	int s, i, opts, type, domain, proto, flags;
 	unsigned int seed, occ;
+    const int types[] = {
+        SOCK_DGRAM,
+        SOCK_STREAM,
+        SOCK_PACKET,
+        SOCK_SEQPACKET,
+        SOCK_RDM,
+    };
+    const int protos[] = {
+        IPPROTO_IP,
+        IPPROTO_ICMP,
+        IPPROTO_IGMP,
+        IPPROTO_IPIP,
+        IPPROTO_TCP,
+        IPPROTO_EGP,
+        IPPROTO_PUP,
+        IPPROTO_UDP,
+        IPPROTO_IDP,
+        IPPROTO_DCCP,
+        IPPROTO_RSVP,
+        IPPROTO_GRE,
+        IPPROTO_IPV6,
+        IPPROTO_ESP,
+        IPPROTO_AH,
+        IPPROTO_BEETPH,
+        IPPROTO_PIM,
+        IPPROTO_COMP,
+        IPPROTO_SCTP,
+        IPPROTO_UDPLITE,
+    };
+    const int domains[] = {
+        AF_UNSPEC,
+        AF_UNIX,
+        AF_LOCAL,
+        AF_INET,
+        AF_AX25,
+        AF_IPX,
+        AF_APPLETALK,
+        AF_NETROM,
+        AF_BRIDGE,
+        AF_ATMPVC,
+        AF_X25,
+        AF_INET6,
+        AF_ROSE,
+        AF_DECnet,
+        AF_NETBEUI,
+        AF_SECURITY,
+        AF_KEY,
+        AF_NETLINK,
+        AF_ROUTE,
+        AF_PACKET,
+        AF_ASH,
+        AF_ECONET,
+        AF_ATMSVC,
+        AF_RDS,
+        AF_SNA,
+        AF_IRDA,
+        AF_PPPOX,
+        AF_WANPIPE,
+        AF_LLC,
+        AF_CAN,
+        AF_TIPC,
+        AF_BLUETOOTH,
+        AF_IUCV,
+        AF_RXRPC,
+        AF_ISDN,
+        AF_PHONET
+    };
 
-	seed = getpid();
+	seed = getpid() ^ time(NULL);
 	occ = 5000000;
 	opts = 10;
-	
+
     signal(SIGPIPE, SIG_IGN);
 
-	while ((c = getopt(ac, av, "r:o:n:")) != EOF)
+	while ((c = getopt(ac, av, "r:o:n:f")) != EOF)
 	{
 		switch (c)
 		{
@@ -358,6 +635,14 @@ int main(int ac, char **av)
 		case 'n':
 			occ = atoi(optarg);
 			break;
+        case 'f':
+            s = open(SEED_FILE, O_RDONLY);
+            if ( s > 0 )
+            {
+                read(s, &seed, sizeof(seed));
+                close(s);
+            }
+            break;
 		case 'h':
 			usage(av[0]);
 			break;
@@ -368,6 +653,13 @@ int main(int ac, char **av)
 	}
 
 	printf(" + using seed: %u\n", seed);
+    s = open(SEED_FILE, O_WRONLY|O_CREAT, 0666);
+    if ( s > 0 )
+    {
+        /* l33t */
+        write(s, &seed, sizeof(seed));
+        close(s);
+    }
 	srand(seed);
 
 	while (occ)
@@ -433,27 +725,52 @@ int main(int ac, char **av)
             case 13:
                 s = socket(AF_X25, SOCK_SEQPACKET, 0);
                 break;
+            case 14:
+                s = socket(AF_PACKET, SOCK_DGRAM, 0);
+                break;
+            case 15:
+                s = socket(AF_PACKET, SOCK_PACKET, 0);
+                break;
+            case 16:
+                s = socket(AF_PACKET, SOCK_SEQPACKET, 0);
+                break;
             default:
-                s = socket(evilint(), evilint(), evilint());
+                domain = domains[rand() % (sizeof(domains)/sizeof(domains[0]))];
+                proto = protos[rand() % (sizeof(protos)/sizeof(protos[0]))];
+                type = types[rand() % (sizeof(types)/sizeof(types[0]))];
+                s = socket(domain, type, proto);
                 break;
         }
 		if (s == -1) continue;
 
+        if (snum > 16)
+            printf("\nsocket(%d, %d, %d);\n", domain, type, proto);
+
+        flags = fcntl(s, F_GETFL, 0);
+        fcntl(s, F_SETFL, flags | O_NONBLOCK);
+
+        if (rand() % 4)
+            connectusse(s);
+        else
+            bindusse(s);
 		for (i = 0; i < opts; i++)
 		{
 			soopt(s);
 			goof(s);
+            sendmsgusse(s);
             usleep(500);
 		}
+        splicusse(s);
         gsn(s);
-        gpn(s);
+        //gpn(s);
+        mmapusse(s);
         printf(".");fflush(stdout);
 		close(s);
 
 #ifdef __LINUX__
         if ( linux_hasoopsed() )
         {
-            printf("dude, check your kernel! It has oopsed\n");
+            printf("\ndude, check your kernel! It has oopsed\n");
             getchar();
         }
 #endif
@@ -461,6 +778,5 @@ int main(int ac, char **av)
 		occ--;
 	}
 
-	printf(" + finished.\n");
 	exit(EXIT_SUCCESS);
 }
